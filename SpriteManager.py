da13ae025be25a0b8f9316829314def023fb084a
Joshua Sterner <joshua.s.sterner@gmail.com>
2019-05-03 00:53:03 -0700

    Added some more test images, also added initial version of SpriteManager.py

---
+++ SpriteManager.py
@@ -0,0 +1,15 @@
+class Sprite:
+    def __init__(self):
+        self.width = 0
+        self.height = 0
+        self.x = 0
+        self.y = 0
+        self.spritemap = None
+        self.spritemap_x = 0
+        self.spritemap_y = 0
+    
+class SpriteMap:
+
+class SpriteManager:
+    def render(self):
+
