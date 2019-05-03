9a2f9477f655526bdb90b142e4ca3b7ec3919a29
Joshua Sterner <joshua.s.sterner@gmail.com>
2019-05-02 21:57:59 -0700

    Added OpenGLTestCaseExample2.py which demonstrates loading textures in OpenGL.

--- OpenGLTestCaseExample.py
+++ OpenGLTestCaseExample.py
@@ -31,19 +31,34 @@ def renderTriangle():
     glVertexPointer(number_of_values_per_vertex, GL_FLOAT, size_of_vertex_in_bytes, offset_into_vertex_data)
 
     # Modern OpenGL Requires a minimum of a vertex shader and a fragment shader rendering
+    
+    # For 2d rendering the vertex shader's main responsibility is to pass the the vertex
+    # position and attributes to the next shader.
     vertex_shader = shaders.compileShader("""
         #version 330
-        in vec2 pos;
+        in vec2 pos; // OpenGL automatically sends the vertex position to this variable in our case
+        out vec2 uv; // create an output variable
         void main() {
-            gl_Position = vec4(pos, 0, 1);   
+            // tell OpenGL where the vertex should be
+            gl_Position = vec4(pos, 0, 1); // 4d vectors in GLSL have the form (x, y, z, w)
+                // the w coordinate of gl_Position was set to 1 essentially because it is a point,
+                // not a direction vector. (you can read up on homogeneous coordinates and 4d
+                // transformation matrices for more detail on this)
+
+            uv = (pos + vec2(1))/2; // scale the vertex coordinates to the range 0-1
         }
         """, GL_VERTEX_SHADER)
 
+    # The fragment shader runs once per fragment. 
+    # A fragment is a pixel-sized (or sometimes smaller) piece of a primitive.
     fragment_shader = shaders.compileShader("""
         #version 330
-        out vec4 fragColor;
+        out vec4 fragColor; // OpenGL automatically recognizes this as our color output
+        in vec2 uv;
         void main() {
-            fragColor = vec4(0.0, 1.0, 0.0, 1.0);
+            // Set the color of the fragment
+            fragColor = vec4(uv, 0.0, 1.0);
+            // fragColor expects a color of the form (red, green, blue, alpha);
         }
     """, GL_FRAGMENT_SHADER)
     
@@ -71,7 +86,7 @@ class OpenGLTestCaseExample(OpenGLTestCase.OpenGLTestCase):
         # prepare to render
         self.set_render_size(128, 128)
     
-        # attempt to render a green triangle
+        # attempt to render a triangle
         renderTriangle()
 
         # wait for a key press to continue
@@ -85,10 +100,11 @@ class OpenGLTestCaseExample(OpenGLTestCase.OpenGLTestCase):
         #   uses a coordinate system where y increases from top to bottom.)
         expected = pygame.image.tostring(expected_image, "RGB", True)
 
-        self.assertEqual(expected, self.get_rendered_image())
-    
         # save the rendered image to test_results/OpenGLTestCaseExample/test_render_triangle.png
+        # note that this must be called before any assert statements or it may not be reached if the test fails.
         self.save_rendered_image()
+        
+        self.assertEqual(expected, self.get_rendered_image())
 
 if __name__ == "__main__":
     unittest.main()
---
+++ OpenGLTestCaseExample2.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python
+
+import pygame
+from OpenGL.GL import *
+from OpenGL.GL import shaders
+import unittest
+import OpenGLTestCase
+import numpy as np
+from ctypes import sizeof, c_float, c_void_p
+
+def renderImage(image):
+    # using resources in open gl generally follows the form of generate, bind, modify
+    
+    # Generate: request a buffer for our vertices
+    vbo = glGenBuffers(1)
+    
+    # Bind: set the newly requested buffer as the active GL_ARRAY_BUFFER. 
+    #   All subsequent modifications of GL_ARRAY_BUFFER will affect our vbo
+    glBindBuffer(GL_ARRAY_BUFFER, vbo)
+
+    # Modify: Tell OpenGL to load data into the buffer. 
+    
+    # I've added two more coordinates to each vertex here for determining the position within the texture.
+    # These two additional coordinates are typically refered to as uv coordinates.
+    # Also there are now two triangles that cover the entire viewport.
+    vertex_data = np.array([-1, -1, 0, 0,  -1, 1, 0, 1,  1, 1, 1, 1,  -1, -1, 0, 0,  1, 1, 1, 1,  1, -1, 1, 0], np.float32)
+    glBufferData(GL_ARRAY_BUFFER, vertex_data, GL_STATIC_DRAW)
+
+    vertex_position_attribute_location = 0
+    uv_attribute_location = 1
+    
+    # glVertexAttribPointer basically works in the same way as glVertexPointer with two exceptions:
+    #   First, it can be used to set the data source for any vertex attributes.
+    #   Second, it has an option to normalize the data, which I have set to GL_FALSE.
+    glVertexAttribPointer(vertex_position_attribute_location, 2, GL_FLOAT, GL_FALSE, sizeof(c_float)*4, c_void_p(0))
+    # vertex attributes need to be enabled
+    glEnableVertexAttribArray(0)
+    glVertexAttribPointer(uv_attribute_location, 2, GL_FLOAT, GL_FALSE, sizeof(c_float)*4, c_void_p(sizeof(c_float)*2))
+    glEnableVertexAttribArray(1)
+
+    # Generate: request a texture
+    image_texture = glGenTextures(1)
+
+    # Bind: set the newly requested texture as the active GL_TEXTURE_2D.
+    #   All subsequent modifications of GL_TEXTURE_2D will affect our texture (or how it is used)
+    glBindTexture(GL_TEXTURE_2D, image_texture)
+    
+    
+    width = image.get_width()
+    height = image.get_height()
+
+    # retrieve a byte string representation of the image.
+    # The 3rd parameter tells pygame to return a vertically flipped image, as the coordinate system used
+    # by pygame differs from that used by OpenGL
+    image_data = pygame.image.tostring(image, "RGBA", True)
+
+    # Modify: Tell OpenGL to load data into the image
+    mip_map_level = 0
+    glTexImage2D(GL_TEXTURE_2D, mip_map_level, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image_data)
+
+    # set the filtering mode for the texture
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+
+    vertex_shader = shaders.compileShader("""
+        #version 330
+        layout(location = 0) in vec2 pos;
+        layout(location = 1) in vec2 uvIn;
+        out vec2 uv;
+        void main() {
+            gl_Position = vec4(pos, 0, 1);
+            uv = uvIn;
+        }
+        """, GL_VERTEX_SHADER)
+
+    fragment_shader = shaders.compileShader("""
+        #version 330
+        out vec4 fragColor;
+        in vec2 uv;
+        uniform sampler2D tex;
+        void main() {
+            fragColor = texture(tex, uv);
+        }
+    """, GL_FRAGMENT_SHADER)
+    
+    shader_program = shaders.compileProgram(vertex_shader, fragment_shader)
+
+
+    glEnableClientState(GL_VERTEX_ARRAY)
+
+    # Enable alpha blending
+    glEnable(GL_BLEND)
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+    
+    glUseProgram(shader_program)
+
+    glDrawArrays(GL_TRIANGLES, 0, 6)
+
+def wait_for_keypress():
+    key_pressed = False
+    while not key_pressed:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                key_pressed = True
+    
+class OpenGLTestCaseExample2(OpenGLTestCase.OpenGLTestCase):
+
+    def test_render_image(self):
+        image = pygame.image.load("test.png")
+        self.set_render_size(image.get_width(), image.get_height())
+        
+        renderImage(image)
+
+        # wait for a key press to continue
+        pygame.display.flip()
+        wait_for_keypress()
+        
+        # load an image to compare the result with
+        expected_image = pygame.image.load("test_render_image_expected.png")
+        
+        # convert expected image to a byte string with format RGB. Also flip the image vertically.
+        #   (OpenGL uses a coordinate system where the y value increases from bottom to top, but pygame
+        #   uses a coordinate system where y increases from top to bottom.)
+        expected = pygame.image.tostring(expected_image, "RGB", True)
+
+        # save the rendered image to test_results/OpenGLTestCaseExample/test_render_triangle.png
+        # note that this must be called before any assert statements or it may not be reached if the test fails.
+        self.save_rendered_image()
+        
+        self.assertEqual(expected, self.get_rendered_image())
+
+    def test_render_image_overlay(self):
+        image = pygame.image.load("colorGrid.png")
+        self.set_render_size(image.get_width(), image.get_height())
+
+        renderImage(image)
+
+        overlayImage = pygame.image.load("test.png")
+        renderImage(overlayImage)
+
+        pygame.display.flip()
+        wait_for_keypress()
+
+if __name__ == "__main__":
+    unittest.main()
+    
