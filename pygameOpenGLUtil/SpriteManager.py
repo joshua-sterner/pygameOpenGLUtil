8a2c4f60a1bcd117a30d59b747c110eaad791074
Luke Kelly <Lukejelly@outlook.com>
2019-07-05 12:20:53 -0700

    Added ability to check if sprite in dict or spritemap in dict. Also changed if blocks to try blocks to improve readability and speed.

--- pygameOpenGLUtil/SpriteManager.py
+++ pygameOpenGLUtil/SpriteManager.py
@@ -20,23 +20,32 @@ class SpriteManager:
     def remove_sprite(self, sprite):
         """Removes a Sprite object from this SpriteManager."""
         sprite_map = sprite.spritemap #Stores the Key that should be in the dict.
-        
-        #Check if the spritemap is in the dictionary.
+        #Check if spritemap in dictionary
         if(sprite_map in self.sprites):
             #Check if sprite is in the set.
-            if(sprite in self.sprites[sprite_map]):
-                #Remove sprite
-                self.sprites[sprite_map].discard(sprite)
-                #Set sprite.manager to None so the Sprite manager updates
-                sprite.manager = None
-                #Check if that was the last sprite in the set.
-                if(len(self.sprites[sprite_map]) == 0):
-                    del self.sprites[sprite_map]
+            try:
+                self.sprites[sprite_map].remove(sprite)
+            except KeyError:
+                raise KeyError("Sprite not in the SpriteManager")
+            #Set Sprite's Sprite Manager to None.
+            sprite.manager = None
+            #Check if this is the last sprite in the set. If so delete the key.
+            if(len(self.sprites[sprite_map]) == 0):
+                del self.sprites[sprite_map]
+        else:
+            raise KeyError("SpriteMap not in Sprite Manager")
 
-                
 
     def _spritemap_changed(self, sprite, old_spritemap):
         pass
 
-    def _sprite_in_dict(self, sprite):
+    #Checks if sprite map in dictionary.
+    def _sprite_map_in_dict(self, sprite):
         return sprite.spritemap in self.sprites
+    
+    #Checks if sprite is in the set represented by it's spritemap
+    def _sprite_in_dict(self, sprite):
+        if(sprite.spritemap in self.sprites):
+            return sprite in self.sprites[sprite.spritemap]
+        else:
+            return False
