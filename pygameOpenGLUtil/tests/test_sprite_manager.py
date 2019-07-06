9cc0a89dac3f23c7ffbb6c01d1a91808a4550364
Luke Kelly <Lukejelly@outlook.com>
2019-07-05 17:20:37 -0700

    Added some local variables for readabilty, added a test_spritemap_changed to test new logic in SpriteManager.py

--- pygameOpenGLUtil/tests/test_sprite_manager.py
+++ pygameOpenGLUtil/tests/test_sprite_manager.py
@@ -31,50 +31,76 @@ def baseline():
 
 
 def test_add_sprite(baseline):
+    #Assign variables for readability. 
+    sprite_manager = baseline[0]
+    sprite_one = baseline[1][0]
+    sprite_two = baseline[1][1]
+    #This sprite has a different sprite map then the other two.
+    sprite_three = baseline[1][2]
+
+
     # Check if you can add a sprite
-    baseline[0].add_sprite(baseline[1][0])
-    assert (baseline[0].has_sprite(baseline[1][0])) == True
+    sprite_manager.add_sprite(sprite_one)
+    assert (sprite_manager.has_sprite(sprite_one)) == True
 
     # Check if you can add another sprite from the same sprite map
-    baseline[0].add_sprite(baseline[1][1])
-    assert (baseline[0].has_sprite(baseline[1][1])) == True
+    sprite_manager.add_sprite(sprite_two)
+    assert (sprite_manager.has_sprite(sprite_two)) == True
 
     # Check if you can add another sprite from a different sprite map
-    baseline[0].add_sprite(baseline[1][2])
-    assert (baseline[0].has_sprite(baseline[1][2])) == True
+    sprite_manager.add_sprite(sprite_three)
+    assert (sprite_manager.has_sprite(sprite_three)) == True
 
 
 def test_remove_sprite(baseline):
+    #Assign variables for readability. 
+    sprite_manager = baseline[0]
+    sprite_one = baseline[1][0]
+
     # Add a sprite
-    baseline[0].add_sprite(baseline[1][0])
+    sprite_manager.add_sprite(sprite_one)
     # Remove Sprite
-    baseline[0].remove_sprite(baseline[1][0])
+    sprite_manager.remove_sprite(sprite_one)
     # Check if Sprite is still there
-    assert (baseline[0].has_sprite(baseline[1][0])) == False
+    assert (sprite_manager.has_sprite(sprite_one)) == False
     # Check if Sprite Manger = None for the sprite removed.
-    assert (baseline[1][0].manager) == None
+    assert (sprite_one.manager) == None
 
     # After removing the only sprite with that sprite map check if spritemap is
     # still a key value in the dict.
-    assert (baseline[0].has_spritemap(baseline[1][0])) == False
+    assert (sprite_manager.has_spritemap(sprite_one)) == False
 
     # Try and remove something that doesn't exist. This should throw a key
     # error, if it does not pytest will fail.
     with pytest.raises(KeyError):
-        baseline[0].remove_sprite(baseline[1][0])
+        sprite_manager.remove_sprite(sprite_one)
 
 
 def test_spritemap_changed(baseline):
-	# Add Sprites
-    baseline[0].add_sprite(baseline[1][0])
+    #Assign variables for readability. 
+    sprite_manager = baseline[0]
+    sprite_one = baseline[1][0]
+    sprite_two = baseline[1][1]
+    #This sprite has a different sprite map then the other two.
+    sprite_three = baseline[1][2]
 
-    old_spritemap = baseline[1][0].spritemap
+    # Add Sprites
+    sprite_manager.add_sprite(sprite_one)
+
+    old_spritemap = sprite_one.spritemap
 
     # Create a new sprite map.
     # This is not an actaul spritemap.
     image_path = str(Path(__file__).parent/"test3.png")
-    spritemap_2 = pygame.image.load(image_path)
+    new_spritemap = pygame.image.load(image_path)
 
     # Change spritemap
-    baseline[1][0].spritemap = spritemap_2
-	
+    sprite_one.spritemap = new_spritemap
+
+    # Check Sprite's Key has changed in Sprite Manager
+    assert (sprite_one in sprite_manager._sprites[old_spritemap]) == False
+
+    # Check if Sprite's Key in Sprite Manager is new Sprite Map
+    assert (sprite_one in 
+            sprite_manager._sprites[new_spritemap]) == True
+    
