30ea427e43074bd3e536e9a472d20eccd2073195
Joshua Sterner <joshua.s.sterner@gmail.com>
2019-07-23 12:57:34 -0700

    Added get_rendered_image(), is_rendered_image(image), and save_image(filename) to OpenGLTestUtils.py. Wrote initial tests for SpriteRenderer. Removed SpriteRenderer_t.py

--- pygameOpenGLUtil/tests/OpenGLTestUtils.py
+++ pygameOpenGLUtil/tests/OpenGLTestUtils.py
@@ -11,3 +11,20 @@ def openGLTestSetup(width, height):
                             pygame.HWSURFACE)
     glViewport(0, 0, width, height)
 
+def get_rendered_image():
+    """Returns the image rendered by opengl as a one-dimensional
+    array of bytes in RGB format"""
+    x, y, width, height = glGetInteger(GL_VIEWPORT)
+    return glReadPixels(x, y, x + width, y + height, GL_RGB,
+                        GL_UNSIGNED_BYTE)
+
+def is_rendered_image(image):
+    rendered_image_bytes = get_rendered_image()
+    image_bytes = pygame.image.tostring(image, "RGB", True)
+    return rendered_image_bytes == image_bytes
+
+def save_image(filename):
+    result = get_rendered_image()
+    x, y, width, height = glGetInteger(GL_VIEWPORT)
+    result_image = pygame.image.fromstring(result, (width, height), "RGB",                                            True)
+    pygame.image.save(result_image, filename)
---
+++ pygameOpenGLUtil/tests/test_SpriteRenderer.py
@@ -0,0 +1,102 @@
+import pygame
+import pytest
+from OpenGL.GL import *
+from pygameOpenGLUtil import *
+from pathlib import Path
+from OpenGLTestUtils import *
+
+
+def load_image(filename):
+    filepath = str(Path(__file__).parent/"images"/filename)
+    return pygame.image.load(filepath)
+
+color_grid_image_path = str(Path(__file__).parent/"images/colorGrid.png")
+inverted_color_grid_image_path = str(Path(__file__).parent/"images/colorGridInverted.png")
+test_image_path = str(Path(__file__).parent/"images/test.png")
+
+def test_render_one_sprite():
+    expected_image = pygame.image.load(color_grid_image_path)
+    openGLTestSetup(1024, 1024)
+    spritemap = Texture(expected_image)
+    sprite_manager = SpriteManager()
+    sprite = Sprite(spritemap)
+    sprite.width = 1024
+    sprite.height = 1024
+    sprite_manager.add_sprite(sprite)
+    sprite_renderer = SpriteRenderer(sprite_manager)
+    sprite_renderer.render()
+    save_image("test_results/test_render_one_sprite.png")
+    assert(is_rendered_image(expected_image))
+
+def test_render_two_adjacent_sprites_from_same_spritemap():
+    expected_image = load_image("test_render_two_adjacent_sprites_from_same_spritemap.png")
+    openGLTestSetup(256, 128)
+    color_grid_image = pygame.image.load(color_grid_image_path)
+    spritemap = Texture(color_grid_image)
+    sprite_manager = SpriteManager()
+    sprite_a = Sprite(spritemap)
+    sprite_a.width = 128
+    sprite_a.height = 128
+    sprite_manager.add_sprite(sprite_a)
+    sprite_b = Sprite(spritemap)
+    sprite_b.width = 128
+    sprite_b.height = 128
+    sprite_b.x = 128
+    sprite_b.spritemap_x = 256
+    sprite_b.spritemap_y = 128
+    sprite_manager.add_sprite(sprite_b)
+    print(sprite_manager.sprites)
+    sprite_renderer = SpriteRenderer(sprite_manager)
+    sprite_renderer.render()
+    save_image("test_results/test_render_two_adjacent_sprites_from_same_spritemap.png")
+    assert(is_rendered_image(expected_image))
+
+def test_render_two_adjacent_sprites_from_different_spritemaps():
+    expected_image = load_image("test_render_two_adjacent_sprites_from_different_spritemaps.png")
+    openGLTestSetup(256, 128)
+
+    sprite_manager = SpriteManager()
+    image0 = pygame.image.load(color_grid_image_path)
+    image1 = pygame.image.load(inverted_color_grid_image_path)
+    texture0 = Texture(image0)
+    texture1 = Texture(image1)
+    sprite0 = Sprite(texture0)
+    sprite0.width = 128
+    sprite0.height = 128
+    sprite0.spritemap_x = 768
+    sprite0.spritemap_y = 256
+    sprite1 = Sprite(texture1)
+    sprite1.width = 128
+    sprite1.height = 128
+    sprite1.spritemap_x = 640
+    sprite1.spritemap_y = 768
+    sprite1.x = 128
+    sprite_manager.add_sprite(sprite0)
+    sprite_manager.add_sprite(sprite1)
+    sprite_renderer = SpriteRenderer(sprite_manager)
+    sprite_renderer.render()
+    save_image("test_results/test_render_two_adjacent_sprites_from_different_spritemaps.png")
+    assert(is_rendered_image(expected_image))
+
+def test_render_overlapping_sprites_with_alpha():
+    expected_image = load_image("test_render_overlapping_sprites_with_alpha.png")
+    openGLTestSetup(1024, 1024)
+    sprite_manager = SpriteManager()
+    image0 = pygame.image.load(color_grid_image_path)
+    image1 = pygame.image.load(test_image_path)
+    texture0 = Texture(image0)
+    texture1 = Texture(image1)
+    sprite0 = Sprite(texture0)
+    sprite0.width = 1024
+    sprite0.height = 1024
+    sprite1 = Sprite(texture1)
+    sprite1.width = 512
+    sprite1.height = 319
+    sprite1.z = -0.1
+    sprite_manager.add_sprite(sprite0)
+    sprite_manager.add_sprite(sprite1)
+    sprite_renderer = SpriteRenderer(sprite_manager)
+    sprite_renderer.render()
+    save_image("test_results/test_render_overlapping_sprites_with_alpha.png")
+    assert(is_rendered_image(expected_image))
+
